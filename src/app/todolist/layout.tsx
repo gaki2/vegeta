1fa3ee1b12b2d6edf276cfade46b10826222b086
유병각 <bgyu@miridih.com>
2024-07-16 19:01:01 +0900

    feat: 좌패널 크기를 리사이즈 하는 기능 추가
    - 리렌더링을 없애기 위해서 useRef 사용함

--- src/app/todolist/layout.tsx
+++ src/app/todolist/layout.tsx
@@ -1,32 +1,40 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useHover } from 'usehooks-ts';
 import { useResize } from './useResize';
+import { clamp } from 'es-toolkit';
 
-const MIN_WIDTH = '248';
-const MAX_WIDTH = '360';
+const MIN_WIDTH = 248;
+const MAX_WIDTH = 412;
 
 export default function TodoListLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const wrapperRef = useRef();
-  const controllerRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const controllerRef = useRef<HTMLDivElement | null>(null);
   const isHover = useHover(controllerRef);
-  const isPointerDown = useResize(controllerRef, ({ dx, dy }) => {
-    console.log(dx, dy);
+  const startWidth = useRef<number>(0);
+  useResize(controllerRef, {
+    pointerDown: () => {
+      startWidth.current = wrapperRef.current!.clientWidth;
+    },
+    pointerMove: ({ dx, dy }) => {
+      const newWidth = clamp(startWidth.current + dx, MIN_WIDTH, MAX_WIDTH);
+      wrapperRef.current!.style.width = `${newWidth}px`;
+    },
+    pointerUp: () => {},
   });
-
-  console.log(isPointerDown);
 
   return (
     <div
       aria-label='투두리스트 좌패널'
       className='flex h-full w-full flex-row'>
       <div
-        className={`relative flex w-[248px] flex-row border-r-[1px] bg-stone-200 ${isHover ? 'border-stone-400' : 'border-stone-300'}`}>
+        ref={wrapperRef}
+        className={`relative flex w-[248px] flex-shrink-0 flex-row border-r-[1px] bg-stone-200 ${isHover ? 'border-stone-400' : 'border-stone-300'}`}>
         <div
           ref={controllerRef}
           aria-label='리사이즈 컨트롤'
